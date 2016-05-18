3fb65499e535ff895b3a16e7ac6b42bd1333bc87
dragonpython <dragonpython@gmail.com>
2016-05-18 11:59:00 +0300

    acopy: basic copy function. Prior to testing

--- basic.py
+++ basic.py
@@ -5,27 +5,28 @@ from shutil import copy2
 #
 
 
-def getfilepath (somedir, filename):
-    result = os.path.join (somedir, filename)
-    return result
 
 
 '''
 dir1 - source directory
     - in some cases can be current directory
 dir2 - destination directory
-filenames = a list of strings
+filenames = a list of filenames
+
+
 '''
 def acopy (dir1, dir2, filenames):
 
-    #forming a list of filepaths:
-    filepaths  = []
+    ##forming a list of filepaths:
+    filepaths  = []# list of full paths to files to copy
     for i in filenames:
-        filepaths.append (getfilepath(dir1, i))
+        filepaths.append (os.path.join (dir1, i)
     
-    print (filepaths)
     
-    #copying each file
+    ##copying each file
+    for i in filepaths:
+        shutil.copy2(i, dir2)
+
 
     
 
